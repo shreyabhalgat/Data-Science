940985566e95a09afc44b6418dccfec35fc42aab
Shreya Bhalgat <bhalgatshreya2@gmail.com>
2022-06-14 16:08:17 +0530

    added variable and operator files in R

---
+++ R/Operators.R
@@ -0,0 +1,30 @@
+#logical Operators
+ #and - Returns true if both the conditions are true else returns false
+ #or  - Returns True if onee of the condition is true
+#! = takes each element of the vector and gives the opoosite logical value
+ #ex - !(10==3)
+  #[1]TRUE
+
+#Arthmatic Operators
+a<-10
+b<-20
+a+b
+a-b
+a*b
+a/b
+a**b
+
+
+#orders of operations
+#parenthesis()
+#exponent()
+#multi and div (left to right)
+
+#rational/logical Operator
+#greater than
+#less than
+#equal to  n so
+
+a>b
+a<b
+a==b
---
+++ R/Variables_and_data_types.R
@@ -0,0 +1,29 @@
+#features of R
+
+#open Source
+#easy to understand
+#varity of inbuilt packages
+
+#Variable in R
+#used to store data with names location
+#combo of letters,digits,periods and underscore
+
+#R is case sensitive
+ #<- works as = known as assignment operator
+
+model1<- 'hello'
+typeof(model1)
+class(model1)
+print(model1)
+
+#1num=shreya //not valid
+
+#DATA TYPES IN R
+#logical -> True and False
+#Numeric -> 10.5,7,845
+#Integer->3l,40l,4l
+#complex -> 3+2i
+#character -> "hello"
+#raw-> "Hello" is stored as 48 65 6c 6c 6f
+
+
