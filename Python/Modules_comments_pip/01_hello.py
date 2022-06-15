2d9766ad5e14eacde8d82a3423f7266429a7d1eb
Shreya Bhalgat <bhalgatshreya2@gmail.com>
2022-06-15 11:48:12 +0530

    added vectors paste paste0 CSV reading and variable n data types in py

---
+++ Python/Modules_comments_pip/01_hello.py
@@ -0,0 +1,11 @@
+print("hello world")
+# modules are inbuilt packages
+#some modules are inbuilt some modules we have to built
+#there are two types of modules
+#inbuilt and external
+#pip in python package manager used to download external packages
+ 
+ 
+'''single line commnet #
+multiple line comment - '''
+
---
+++ Python/Variable_and_data_types/01_variable.py
@@ -0,0 +1,13 @@
+'''variable is name given to storage'''
+
+a="shreya"
+b=45
+c=35.42
+
+'''Data types
+1.integers
+2.floating
+3.string
+4.Booleans
+5.None
+'''
